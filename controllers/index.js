c7cf79665bcb5826f14b63e60eadddc97e4b1222
WesleyLere <111530485+WesleyLere@users.noreply.github.com>
2022-12-19 14:45:37 -0600

    handlebars and other miscellaneous stuff

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const homeRoutes = require('./homeRoutes.js');
+
+router.use('/', homeRoutes);
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
