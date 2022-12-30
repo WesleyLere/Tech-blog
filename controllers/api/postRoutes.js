e4c771ad56774f64e87d9779e3d728cd68f9c8f8
WesleyLere <111530485+WesleyLere@users.noreply.github.com>
2022-12-30 14:50:03 -0600

    comment page works now/ displays names on posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,18 +2,39 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/new', withAuth,  async (req, res) => {
+router.post('/new', withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
       ...req.body,
       user_id: req.session.user_id,
     })
     res.status(200).json(newPost);
-    res.render('/')
   } catch (err) {
     res.status(400).json(err);
     console.log(err)
   }
 });
+
+router.put('/update/:id', withAuth, async (req, res) => {
+  try {
+    const updatePost = await
+      Post.update(
+        {
+          title: req.body.title,
+          body: req.body.body,
+        },
+        {
+          where:
+          {
+            id: req.params.id,
+          }
+        }
+      )
+    res.status(200).json(updatePost)
+  } catch (err) {
+    res.status(400).json(err);
+    console.log(err)
+  }
+})
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,32 +1,33 @@
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
     try {
-        // Get all projects and JOIN with user data
+        // Get all posts and JOIN with user data
         const postData = await Post.findAll({
-          include: [
-            {
-              model: User,
-              attributes: ['username'],
-            },
-          ],
+            include: [
+                {
+                    model: User,
+                    exclude: ['password']
+                },
+            ],
         });
-    
+
         // Serialize data so the template can read it
         const posts = postData.map((post) => post.get({ plain: true }));
-    
+
         // Pass serialized data and session flag into template
         res.render('homepage'
-        , { 
-          posts, 
-          //logged_in: req.session.logged_in 
-        }
+            , {
+                posts,
+                logged_in: req.session.logged_in
+            }
         );
-      } catch (err) {
+    } catch (err) {
         res.status(500).json(err);
-      }
+    }
 })
 
 router.get('/login', async (req, res) => {
@@ -34,18 +35,93 @@ router.get('/login', async (req, res) => {
 
 })
 
-router.get('/dashboard',  (req, res) => {
-    res.render('dashboard')
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        // Get all posts and JOIN with user data
+        const postData = await Post.findAll({
+
+            include: [
+                {
+                    model: User,
+                    exclude: ['password']
+                },
+            ],
+            where: { user_id: req.session.user_id },
+        });
+
+        // Serialize data so the template can read it
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('dashboard'
+            , {
+                posts,
+                logged_in: req.session.logged_in
+            }
+        );
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/addpost', (req, res) => {
+    res.render('addPost')
 
 })
 
-router.get('/addpost',  (req, res) => {
-     res.render('addPost')
+router.get('/post/:id', async (req, res) => {
+    
+    try {
+        console.log('hello world')
+        const singlePostData = await Post.findByPk(req.params.id, {
+            include: [
+                User,
+                {
+                    model: Comment,
+                    include: [User],
+                },
+            ],
+        })
+       
+        const commentData = await Comment.findAll(req.params.post_id, {
+            include: [{
+                model: Post
+            }]
+        })
+  
+    //     const userCommentData = await User.findOne(req.params.user_id, {
+    //         include: [
+    //             User,
+    //             {
+    //                 model: Comment,
+    //                 include: [User],
+    //             },
+    //         ],
+    //   })
+  
+         console.log('commentdata', commentData)
+        // const users = userCommentData.map((username) => username.get({ plain: true }));
+        const singlePost = singlePostData.get({ plain: true });
+        const comments = commentData.map((comment) => comment.get({ plain: true }));
+        res.render('comment'
+            ,
+            {
+                // users,
+                singlePost,
+                comments,
+                logged_in: req.session.logged_in
+            }
+        );
+    } catch (err) {
+        res.json(err)
+    }
+  
+  })
+  
+  router.get('/update', async (req, res) => {
+    await res.render('update')
 
 })
-router.get('/comment',  (req, res) => {
-    res.render('comment')
 
-})
 module.exports = router;
 
